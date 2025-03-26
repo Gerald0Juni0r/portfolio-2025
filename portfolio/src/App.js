d1b62300e6117774343ef9ac0c6fa6bb28e0c2eb
Gerald0Juni0r <geraldo.gj63@gmail.com>
2025-03-26 15:37:57 -0300

    adicionando

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import logo from '../src/logo.png';
 import './App.css';
 
+// Conhecido como JSX
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav className="navigation-area">
+          <div>
+            <a >INÍCIO</a>
+          </div>
+          <div>
+            <a className="navigation-area">SOBRE</a>
+          </div>
+          <div>
+            <a className="navigation-area">PROJETOS</a>
+          </div>
+          <div>
+            <a className="navigation-area">HABILIDADES</a>
+          </div>
+        </nav>
+        <div className="language">
+          <a>eng-us</a>
+        </div>
       </header>
+        <h2>
+          Olá  , eu me chamo
+        </h2>
+        <div className="name">
+          <h1 className="first-name">
+            <span className="letra-cor">G</span>
+            <span className="letra-cor">E</span>
+            <span className="letra-cor">R</span>
+            <span className="letra-cor">A</span>
+            <span className="letra-cor">L</span>
+            <span className="letra-cor">D</span>
+            <span className="letra-cor">O</span>
+          </h1>
+          <h1 className="last-name">
+            <span className="letra-cor">J</span>
+            <span className="letra-cor">Ú</span>
+            <span className="letra-cor">N</span>
+            <span className="letra-cor">I</span>
+            <span className="letra-cor">O</span>
+            <span className="letra-cor-fixa">R</span>
+          </h1>
+        </div>
     </div>
   );
 }
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Tem a função de inicialiazar o React no HTML
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
